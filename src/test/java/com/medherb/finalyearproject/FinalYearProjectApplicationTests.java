8f29e67ed1d2d804f3ff17915ad9c769b9fe7d49
Samuel-2552 <201501502@rajalakshmi.edu.in>
2024-02-14 23:59:31 +0530

    Initial Commit

---
+++ src/test/java/com/medherb/finalyearproject/FinalYearProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medherb.finalyearproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalYearProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
